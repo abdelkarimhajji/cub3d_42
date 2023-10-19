18310a0cd3fdfb4e0698b921437083d1ef5b2e72
Abdlekarim <92029974+karimhajji1@users.noreply.github.com>
2023-10-19 13:08:20 +0100

    Add files via upload

---
+++ cub3d2/cub3d.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahajji <ahajji@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 11:01:47 by ahajji            #+#    #+#             */
+/*   Updated: 2023/10/19 11:09:07 by ahajji           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "./MLX/MLX42.h"
+
+char myMap[11][16] = {
+    "111111111111111",
+    "100000000010101",
+    "100000000001001",
+    "11111000010011",
+    "1000000P1001011",
+    "100000000111111",
+    "100000100000001",
+    "100000000000001",
+    "111111001111111",
+    "100000000000001",
+    "111111111111111"
+};
+
+typedef struct  s_data
+{
+    mlx_t       *mlx;
+    mlx_image_t *img;
+	void	    *mlx_win;
+    void        *mlx_img;
+    double         xp;
+    double         yp;  
+    int         start_x_p;
+    int         start_y_p;
+    double      second_point_x;
+    double      second_point_y;
+    int         corner;
+    
+}               t_data;
