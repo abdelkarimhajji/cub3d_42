a05fe6e1008ee47cc7c0cd5c2fe7866856735db3
abdelkarim <hajji.karim.solicode@gmail.com>
2023-10-25 10:49:49 +0100

    now i add move player front, back , left , right and rotate the angle when the player see and also check walls

--- newCub3d/cub3d.c
+++ newCub3d/cub3d.c
@@ -6,15 +6,167 @@
 /*   By: ahajji <ahajji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 11:18:46 by ahajji            #+#    #+#             */
-/*   Updated: 2023/10/24 11:47:25 by ahajji           ###   ########.fr       */
+/*   Updated: 2023/10/25 10:45:36 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void    draw()
+
+float   to_rad(float degree)
 {
+    return(degree * (M_PI / 180));
+}
+
+void    draw_line_dda(t_cub3d *data, float  x1, float   y1, float  x2, float  y2, uint32_t color)
+{
+    float   dx;
+    float   dy;
+    float   steps;
+    float   increamentx;
+    float   increamenty;
+    int i = 0;
     
+    dx = x2 - x1;
+    dy = y2 - y1;
+    if (fabs(dx) > fabs(dy))
+        steps = fabs(dx);
+    else
+        steps = fabs(dy);
+    increamentx = dx /steps;
+    increamenty = dy / steps;
+    while (i <= steps)
+    {
+        mlx_put_pixel(data->img, round(x1), round(y1), 0xFF0000FF);
+        x1 += increamentx;
+        y1 += increamenty;        
+        i++;
+    }
+}
+
+void    init_data(t_cub3d *data)
+{
+    data->width_map = 15;
+    data->height_map = 11;
+    data->angle = 50;
+}
+
+void    draw_rectangle(int x, int y, t_cub3d *data, uint32_t color)
+{
+    int i;
+    int j;
+	
+    i = x;
+    j = y;
+    while (y <  size_shape + j)
+    {
+        x = i;
+        while (x <  size_shape + i)
+        {
+            mlx_put_pixel(data->img, x, y, color);
+            x++;
+        }
+        y++;
+    }
+}
+
+void draw_map(t_cub3d *data, int mode)
+{
+	int i = 0;
+    int j = 0;
+	
+	while (j < 11)
+    {
+        i = 0;
+        while (i < 16)
+        {
+            if (myMap[j][i] == '1')
+                draw_rectangle( i * size_shape, j * size_shape, data, 0xFFFFFFFF);
+			if (myMap[j][i] == '0' || (mode == 0 && myMap[j][i] == 'P'))
+				draw_rectangle( i * size_shape, j * size_shape, data, 0x000000FF);
+            if(myMap[j][i] == 'P' && mode)
+            {
+				draw_rectangle(i * size_shape, j * size_shape, data, 0x000000FF);
+				mlx_put_pixel(data->img, data->px = (i * size_shape) + 13, data->py = (j * size_shape) + 13, 0xFF0000FF);
+            }
+            i++;
+        }
+        j++;
+    }
+}
+
+int    check_wall(t_cub3d *data)
+{
+    int x;
+    int y;
+    
+    if(mlx_is_key_down(data->mlx, MLX_KEY_W))
+    {
+        x = data->px + (cos(to_rad(data->angle)) * move_step);
+        y = data->py + (sin(to_rad(data->angle)) * move_step);
+    }
+    else if(mlx_is_key_down(data->mlx, MLX_KEY_S))
+    {
+        x = data->px - (cos(to_rad(data->angle)) * move_step);
+        y = data->py - (sin(to_rad(data->angle)) * move_step);
+    }
+    else if(mlx_is_key_down(data->mlx, MLX_KEY_D))
+    {
+        x = data->px - cos(to_rad(90) - to_rad(data->angle)) * move_step;
+	    y = data->py + sin(to_rad(90) - to_rad(data->angle)) * move_step;
+    }
+    else if(mlx_is_key_down(data->mlx, MLX_KEY_A))
+    {
+        x = data->px + cos(to_rad(90) - to_rad(data->angle)) * move_step;
+	    y = data->py - sin(to_rad(90) - to_rad(data->angle)) * move_step;
+    }
+    if(myMap[(int)(y / size_shape)][(int)(x / size_shape)] == '1' || (myMap[(int)(y / size_shape)][(int)(data->px / size_shape)] == '1' && myMap[(int)(data->py / size_shape)][(int)(x / size_shape)] == '1'))
+        return 0;
+    return 1;
+}
+
+void    draw(void   *param)
+{
+    t_cub3d *data = (t_cub3d *)param;
+
+    if(mlx_is_key_down(data->mlx, MLX_KEY_LEFT) )
+    {
+        data->angle += speed_rotate;
+        if(data->angle >= 360)
+            data->angle = 0;
+    }
+    else if(mlx_is_key_down(data->mlx, MLX_KEY_RIGHT)  )
+    {
+        data->angle -= speed_rotate;
+        if (data->angle <= 0)
+            data->angle = 360;
+    }
+    else if(mlx_is_key_down(data->mlx, MLX_KEY_W) && check_wall(data))
+    {
+        data->px = data->px + (cos(to_rad(data->angle)) * move_step);
+        data->py = data->py + (sin(to_rad(data->angle)) * move_step);
+    }
+    else if(mlx_is_key_down(data->mlx, MLX_KEY_S) && check_wall(data))
+    {
+        data->px = data->px - (cos(to_rad(data->angle)) * move_step);
+        data->py = data->py - (sin(to_rad(data->angle)) * move_step);
+    }
+    else if(mlx_is_key_down(data->mlx, MLX_KEY_D) && check_wall(data))
+    {
+        data->px = data->px - cos(to_rad(90) - to_rad(data->angle)) * move_step;
+	    data->py = data->py + sin(to_rad(90) - to_rad(data->angle)) * move_step;
+    }
+    else if(mlx_is_key_down(data->mlx, MLX_KEY_A) && check_wall(data))
+    {
+        data->px = data->px + cos(to_rad(90) - to_rad(data->angle)) * move_step;
+	    data->py = data->py - sin(to_rad(90) - to_rad(data->angle)) * move_step;
+    }
+    // printf("%f\n", data->angle);
+    draw_map(data, 0);
+    mlx_put_pixel(data->img, data->px, data->py, 0xFF0000FF);
+
+    draw_line_dda(data, data->px, data->py, data->px + (cos(to_rad(data->angle)) * 100),
+     data->py + (sin(to_rad(data->angle)) * 100), 0xFF0000FF);
 }
 
 int main(int ac, char **av)
@@ -25,8 +177,10 @@ int main(int ac, char **av)
     if(!data.mlx)
         return (1);
     data.img = mlx_new_image(data.mlx, width_win, height_win);
-    if(!(data.img || mlx_image_to_window(data.mlx, data.img, 0, 0)))
+    if(!data.img || (mlx_image_to_window(data.mlx, data.img, 0, 0)) < 0 )
         return(1);
+    init_data(&data);
+    draw_map(&data, 1);
     mlx_loop_hook(data.mlx, draw, &data);
     mlx_loop(data.mlx);
     mlx_terminate(data.mlx);
--- newCub3d/cub3d.h
+++ newCub3d/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ahajji <ahajji@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 11:19:16 by ahajji            #+#    #+#             */
-/*   Updated: 2023/10/24 11:45:48 by ahajji           ###   ########.fr       */
+/*   Updated: 2023/10/25 10:47:15 by ahajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,34 @@
 #include <math.h>
 #include "./MLX/MLX42.h"
 
+char myMap[11][16] = {
+    "111111111111111",
+    "100000000010101",
+    "100000000001001",
+    "11111000010011",
+    "1000000P1001011",
+    "100000000111111",
+    "100000100000001",
+    "100000000000001",
+    "111111001111111",
+    "100000000000001",
+    "111111111111111"
+};
+
+
 #define width_win  2000
 #define height_win  900
-#define size_shape 25
-
+#define size_shape 26
+#define speed_rotate 0.5
+#define move_step 1
 typedef struct s_cub3d
 {
     mlx_t       *mlx;
     mlx_image_t *img;
+    int         width_map;
+    int         height_map;
     float       px;
     float       py;
     float       angle;
+    
 }               t_cub3d;
