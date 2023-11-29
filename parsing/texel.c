91e7a43e9a1feccf0cfe1e0626529b56dc674444
AchabNassim <achab.nassim.solicode@gmail.com>
2023-11-29 12:38:27 +0100

    .

--- parsing/texel.c
+++ parsing/texel.c
@@ -12,18 +12,18 @@
 
 #include "../cub3d.h"
 
-uint32_t    get_texel(mlx_image_t *img, int x, int y)
+uint32_t	get_texel(mlx_image_t *img, int x, int y)
 {
-    uint8_t *pixel;
-    uint8_t alpha;
-    uint8_t red;
-    uint8_t green;
-    uint8_t blue;
+	uint8_t	*pixel;
+	uint8_t	alpha;
+	uint8_t	red;
+	uint8_t	green;
+	uint8_t	blue;
 
-    pixel = &img->pixels[((y * img->width) + x) * 4];
-    red = pixel[0];
-    green = pixel[1];
-    blue = pixel[2];
-    alpha = pixel[3];
-    return (red << 24 | green << 16 | blue << 8 | alpha);
+	pixel = &img->pixels[((y * img->width) + x) * 4];
+	red = pixel[0];
+	green = pixel[1];
+	blue = pixel[2];
+	alpha = pixel[3];
+	return (red << 24 | green << 16 | blue << 8 | alpha);
 }
